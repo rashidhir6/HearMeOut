7dd46465dd70f94a2c91f9890a5caaf43d2c0649
Hrituja <37546675+Hrituja@users.noreply.github.com>
2018-10-21 11:41:38 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,65 +1,188 @@
-package com.hr.speechtotext;
+package smsreader.hathy.com.newsmsreader;
 
 
-        import android.content.ActivityNotFoundException;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.speech.RecognizerIntent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.ImageButton;
-        import android.widget.TextView;
+import java.lang.Object;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.BroadcastReceiver;
+import android.telephony.SmsMessage;
+import android.provider.ContactsContract.PhoneLookup;
+import android.content.IntentFilter;
+import android.net.Uri;
 
-        import java.util.ArrayList;
-        import java.util.Locale;
+import android.speech.tts.TextToSpeech;
+import android.provider.ContactsContract;
+import android.database.Cursor;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.hathy.newsmsreader.R;
 
-    private static final int REQ_CODE_SPEECH_INPUT = 100;
-    private TextView mVoiceInputTv;
-    private ImageButton mSpeakBtn;
+
+public class MainActivity extends Activity {
+    private final int CHECK_CODE = 0x1;
+    private final int LONG_DURATION = 5000;
+    private final int SHORT_DURATION = 1200;
+
+    private smsreader.hathy.com.newsmsreader.Speaker speaker;
+
+    private ToggleButton toggle;
+    private OnCheckedChangeListener toggleListener;
+
+    private TextView smsText;
+    private TextView smsSender;
+
+    private BroadcastReceiver smsReceiver;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        toggle = (ToggleButton) findViewById(R.id.speechToggle);
+        smsText = (TextView) findViewById(R.id.sms_text);
+        smsSender = (TextView) findViewById(R.id.sms_sender);
 
-        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
-        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
-        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
-
+        toggleListener = new OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                startVoiceInput();
+            public void onCheckedChanged(CompoundButton view, boolean isChecked) {
+                if (isChecked) {
+                    speaker.allow(true);
+                    speaker.speak(getString(R.string.start_speaking));
+                } else {
+                    speaker.speak(getString(R.string.stop_speaking));
+                    speaker.allow(false);
+                }
             }
-        });
+        };
+        toggle.setOnCheckedChangeListener(toggleListener);
+
+        checkTTS();
+        initializeSMSReceiver();
+        registerSMSReceiver();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void startVoiceInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(smsReceiver);
+        speaker.destroy();
+    }
 
-        }
+    private void checkTTS() {
+        Intent check = new Intent();
+        check.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(check, CHECK_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    mVoiceInputTv.setText(result.get(0));
-                }
-                break;
+        if (requestCode == CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                speaker = new Speaker(this);
+            } else {
+                Intent install = new Intent();
+                install.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(install);
             }
-
         }
     }
+
+    private void initializeSMSReceiver() {
+        smsReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                Bundle bundle = intent.getExtras();
+                if (bundle != null) {
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    for (int i = 0; i < pdus.length; i++) {
+                        byte[] pdu = (byte[]) pdus[i];
+                        SmsMessage message = SmsMessage.createFromPdu(pdu);
+                        String text = message.getDisplayMessageBody();
+                        String sender = getContactName(message.getOriginatingAddress());
+                        speaker.pause(LONG_DURATION);
+                        speaker.speak("You have a new message from" + sender + "!");
+                        speaker.pause(SHORT_DURATION);
+                        speaker.speak(text);
+                        smsSender.setText("Message from " + sender);
+                        smsText.setText(text);
+                    }
+                }
+
+            }
+        };
+    }
+
+    private String getContactName(String phone) {
+        Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone));
+        String projection[] = new String[]{ContactsContract.Data.DISPLAY_NAME};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor.moveToFirst()) {
+            return cursor.getString(0);
+        } else {
+            return "unknown number";
+        }
+    }
+
+    private void registerSMSReceiver() {
+        IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(smsReceiver, intentFilter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://smsreader.com.ha.newsmsreader/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://smsreader.com.ha.newsmsreader/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
+
